e8a6bd08c0f29429521ed3dfb8ec06ab4a53e66b
Sinder X D SUN <sinder.x.d.sun@noexternalmail.hsbc.com>
2019-05-31 14:03:56 +0800

    Initial Commit

---
+++ inputTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inputTest
+//
+//  Created by Sinder X D Sun on 2019/5/31.
+//  Copyright © 2019 Sinder X D Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
